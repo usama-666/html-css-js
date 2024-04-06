31525ec44ca95626b4db7a1b52aabc58f931ee75
usama-666 <ejazali8582@gmail.com>
2024-04-06 22:17:24 +0500

    do some coding

---
+++ Project-7/index.js
@@ -0,0 +1,9 @@
+function scrolltoTop() {
+  document.body.scrollIntoView({ behavior: "smooth", block: "start" });
+}
+
+console.log("hellowdasda");
+
+function print() {
+  console.log("hellos woed");
+}
